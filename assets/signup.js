5a093f09e9aed343ca277eb070ad5622bbf10afa
Hao <ngoanhhao24@gmail.com>
2022-07-13 16:48:35 +0700

    update

--- assets/signup.js
+++ assets/signup.js
@@ -1,7 +1,7 @@
 // login box
 $(document).ready(function () {
   // create login box
-    $(".background-signup").html(`
+  $(".background-signup").html(`
     <div class="background-signup-content d-flex overflow-hidden">
       <div class="w-50 register flex-column justify-content-center align-items-center">
         <label for="" class="d-block mb-4">
@@ -34,8 +34,8 @@ $(document).ready(function () {
         <button type="button" class="btn btn-primary login-btn bth-signup mt-3">SIGN UP</button>
       </div>
     </div>
-    `)
-    // reset animation login box khi tắt bật lại
+    `);
+  // reset animation login box khi tắt bật lại
   $(".signin").click(function () {
     $(".background-signup").css("display", "flex");
     $(".register2").css("animation", "");
@@ -50,7 +50,7 @@ $(document).ready(function () {
   $(".background-signup").click(function () {
     $(".background-signup").css("display", "none");
   });
-//   animation login box
+  //   animation login box
   $(".register-btn").click(function () {
     if ($(".register-btn").text() == "SIGN UP") {
       $(".register").css("display", "none");
@@ -68,55 +68,93 @@ $(document).ready(function () {
       $(".signupp").css("animation", "LtoR  ease 0.7s");
     }
   });
-
+  if (localStorage.getItem("login") == 1) {
+    signinSuccess();
+  }
   // tạo và kiểm tra username password
-  $(".bth-signup").click(function(){
-      var user = $(".user").val();
-      var pass = $(".pass").val();
-      if(!user || !pass){
-        alert("Write somthing!!");
-      }else{
-          var newguest = new guest(user,pass);
-          if (!localStorage.getItem("guestID")){
-            localStorage.setItem("guestID","1");
-          }else{
-            localStorage.setItem("guestID",parseInt(localStorage.getItem("guestID"))+1)
-          }
-          localStorage.setItem(localStorage.getItem("guestID"),JSON.stringify(newguest));
-          alert("Successful account creation, login please");
-          $(".user").val("");
-          $(".pass").val("");
-          $(".name").val("");
+  $(".bth-signup").click(function () {
+    var user = $(".user").val();
+    var pass = $(".pass").val();
+    if (!user || !pass) {
+      alert("Write somthing!!");
+    } else {
+      var newguest = new guest(user, pass);
+      if (!localStorage.getItem("guestID")) {
+        localStorage.setItem("guestID", "1");
+      } else {
+        localStorage.setItem(
+          "guestID",
+          parseInt(localStorage.getItem("guestID")) + 1
+        );
       }
-    })
-    $(".bth-signin").click(function(){
-      if(!$(".email").val() || !$(".password")){
-        alert("Write somthing!!");
-      }else{
-        for(var i = parseInt(localStorage.getItem("guestID")); i>0 ;i--){
-            var guest2 = JSON.parse(localStorage.getItem(i));
-            if(($(".email").val()!=guest2.user) || ($(".password").val()!=guest2.pass)){
-              $(".errorr").html("Username or Password incorrect.");
-            }else{
-              alert("Login successfully");
-              $(".email").val("");
-              $(".password").val("");
-              $(".background-signup").css("display", "none");
-              $(".errorr").html("");
-              break;
-            }
-          }
-        }
-    })
-// xóa bớt animation trên mobile
-    const mediaQuery = window.matchMedia('(max-width: 576px)')
-    if(mediaQuery.matches){
-      $('div [data-aos="flip-right"]').removeAttr("data-aos");
-      $('div [data-aos="slide-right"]').removeAttr("data-aos");
-      $('div [data-aos="zoom-in"]').removeAttr("data-aos");
+      localStorage.setItem(
+        localStorage.getItem("guestID"),
+        JSON.stringify(newguest)
+      );
+      alert("Successful account creation, login please");
+      $(".user").val("");
+      $(".pass").val("");
+      $(".name").val("");
     }
+  });
+  $(".bth-signin").click(function () {
+    switch (signin()) {
+      case 0:
+        alert("Write somthing!!");
+        break;
+      case 1:
+        $(".errorr").html("");
+        localStorage.setItem("login", "1");
+        location.reload();
+        break;
+    }
+  });
+  //logout
+  $(".logout").click(function(){
+    localStorage.setItem("login","0")
+    location.reload();
+  })
+  // xóa bớt animation trên mobile
+  const mediaQuery = window.matchMedia("(max-width: 576px)");
+  if (mediaQuery.matches) {
+    $('div [data-aos="flip-right"]').removeAttr("data-aos");
+    $('div [data-aos="slide-right"]').removeAttr("data-aos");
+    $('div [data-aos="zoom-in"]').removeAttr("data-aos");
+  }
 });
-function guest(user,pass){
-    this.user = user;
-    this.pass = pass;
+function guest(user, pass) {
+  this.user = user;
+  this.pass = pass;
+}
+
+function signin() {
+  if (!$(".email").val() || !$(".password")) {
+    // alert("Write somthing!!");
+    return 0;
+  } else {
+    for (var i = parseInt(localStorage.getItem("guestID")); i > 0; i--) {
+      var guest2 = JSON.parse(localStorage.getItem(i));
+      if (
+        $(".email").val() != guest2.user ||
+        $(".password").val() != guest2.pass
+      ) {
+        $(".errorr").html("Username or Password incorrect.");
+      } else {
+        return 1;
+      }
+    }
+  }
+}
+
+function signinSuccess() {
+  $(".DN").html(`
+  <img class="dropdown-toggle" width="30%" src="assets/Img/avatar.png" alt=""  id="navbarDropdown"
+  role="button" data-bs-toggle="dropdown" aria-expanded="false"> 
+  
+  <ul class="dropdown-menu account-content">
+    <li><a class="dropdown-item" href="#">My account</a></li>
+    <li><a class="dropdown-item" href="#">Shoping cart</a></li>
+    <li><a class="dropdown-item logout" href="#">Log out</a></li>
+  </ul>
+  `);
 }

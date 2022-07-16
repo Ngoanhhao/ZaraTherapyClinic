ec30aae6a74e7b7f1c77399b4fc9b65f5ceab70b
Hao <ngoanhhao24@gmail.com>
2022-07-16 18:36:01 +0700

    test ascess

---
+++ assets/footer.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  $(".footer").html(`
+    <div class="d-flex pt-5 pb-5 row">
+    <div class="col-3">
+      <img width="50%" src="assets/Img/logov2.png" alt="">
+      <h3>Chiropractic Clinic</h3>
+      <p class="s-14">Provide the best healthcare service for the spine and effectively address musculoskeletal
+        problems without drugs or surgery.</p>
+      <p class="s-12 mt-3">© Copyright 2022 - ZaraTherapyClinic</p>
+    </div>
+    <div class="footer-content col-3">
+      <h3 class="mb-3">Contact Us</h3>
+      <a href="" class="s-14"><i class="fa-regular fa-circle-question"></i> Help center</a>
+      <a href="" class="s-14"><i class="fa-brands fa-facebook"></i> Facebook</a>
+      <a href="" class="s-14"><i class="fa-brands fa-instagram"></i> Instagram</a>
+      <a href="" class="s-14"><i class="fa-regular fa-circle-question"></i> About Us</a>
+    </div>
+    <div class="footer-content col-3">
+      <h3 class="mb-3">Userful Links</h3>
+      <a href="" class="s-14">How It Works</a>
+      <a href="" class="s-14">Reviews</a>
+      <a href="" class="s-14">Term & Conditions</a>
+      <a href="" class="s-14">Privacy Policy</a>
+      <a href="" class="s-14">Site Map</a>
+    </div>
+    <div class="col-3 s-14">
+      <h3 class="mb-3">LOCATIONS</h3>
+      <h4>ZaraTherapyClinic - Da Nang</h4>
+      <p>36 Yen Bai - Da Nang</p>
+      <p>Tel: <a href="tel:+0905622271">+0905 622 271</a></p>
+      <h4 class="d-inline-block mt-5">Ascess:</h4>
+      <span class="d-inline-block count" data-id="1000000">1000</p>
+    </div>
+  </div>
+    `);
+  $(document).scroll(function () {
+    console.log($("html,body").scrollTop())
+    if ($("html,body").scrollTop() > 500) {
+      animationNum(count);
+    }
+  });
+  var endnum = $(".count").text();
+  var count = 0;
+  $(".count").text("0");
+  function animationNum(count) {
+    $(".count").html(count);
+    console.log($(".count").text());
+    if (count < endnum) {
+      setTimeout(() => {animationNum(count + 1)}, 0.001);
+
+    }
+  }
+});

27a94396e1f2287eba8d215502501b8b71469634
Hao <ngoanhhao24@gmail.com>
2022-07-20 22:19:59 +0700

    update product

---
+++ assets/products.js
@@ -0,0 +1,58 @@
+var dola = 1.19;
+    var environs = [
+        {
+            img: "./assets/Img/products/environ/NEW_Environ_Starter_Kit-905x643-7fc31b26-4ed7-432f-a28a-9889b5fed00c.png",
+            saleimg:"./assets/Img/products/environ/RAD-Shield-Mineral-Sunscreen-125ml.png",
+            name: "Environ Starter Kit",
+            price: `${109 * dola}$`,
+            id: 0
+        },
+        {
+            img: "./assets/Img/products/environ/14042016-193659-youth-essentia-eye-gel-905x1280-9aa30ff4-8fe1-4c1f-9742-9dbc0a2f3a1d.png",
+            saleimg:"./assets/Img/products/environ/14042016-193659-youth-essentia-eye-gel-905x1280-9aa30ff4-8fe1-4c1f-9742-9dbc0a2f3a1d (1).png",
+            name: "Environ Vita-Peptide Eye Gel ",
+            price: `${59 * dola}$`
+        },
+        {
+            img: "./assets/Img/products/environ/08092016-174826-skin_essentia_avst_moisturiser_1.png",
+            saleimg:"./assets/Img/products/environ/Environ_Vita-Antioxidant_AVST1_Moisturiser.png",
+            name: "Environ Vita-Antioxidant AVST1 Moisturiser",
+            price: `${50 * dola}$`
+        },
+        {
+            img: "./assets/Img/products/environ/08092016-174856-skin_essentia_avst_moisturiser_3-905x1280-d5d6c9f9-890e-405b-93a5-aacec039672e.png",
+            saleimg:"./assets/Img/products/environ/Environ_Vita-Antioxidant_AVST3_Moisturiser.png",
+            name: "Environ Vita-Antioxidant AVST3 Moisturiser",
+            price: `${50 * dola}$`
+        },
+        {
+            img: "./assets/Img/products/environ/11042018-144009-sebu-wash-gel-cleanser-copy-905x1744-d085b030-26d4-482b-9991-543aaad32520.jpg",
+            saleimg:"./assets/Img/products/environ/Environ_Sebu-Wash.png",
+            name: "Environ Sebu-Wash",
+            price: `${31 * dola}$`
+        },
+        {
+            img: "./assets/Img/products/environ/se_low_foam_cleansing_gel-205x878-ee0c4d18-3c55-44fd-b467-1f97c826b7e5.png",
+            saleimg:"./assets/Img/products/environ/Environ_Low_Foam_Cleansing_Gel.png",
+            name: "Environ Low Foam Cleansing Gel",
+            price: `${39 * dola}$`
+        },
+        {
+            img: "./assets/Img/products/environ/ENV057.jpg",
+            saleimg:"./assets/Img/products/environ/100ml_rad_antioxidant_sun-cream_tube_hr.png",
+            name: "Environ RAD SPF15",
+            price: `${22 * dola}$`
+        },
+        {
+            img: "./assets/Img/products/environ/ENV012.jpg",
+            saleimg:"./assets/Img/products/environ/Environ_Vita-Antioxidant_AVST4_Moisturiser.png",
+            name: "Environ Vita-Antioxidant AVST4 Moisturiser",
+            price: `${57 * dola}$`
+        },
+        {
+            img: "./assets/Img/products/environ/ENV038.jpg",
+            saleimg:"./assets/Img/products/environ/Environ_Vita-Enriched_Colostrum_Gel.png",
+            name: "Environ Vita-Enriched Colostrum Gel",
+            price: `${38 * dola}$`
+        },
+    ]  
